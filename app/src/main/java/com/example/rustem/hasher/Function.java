9e6079e57469a63a28aa59228719a52a8c28e953
rustemazimov <28685606+rustemazimov@users.noreply.github.com>
2017-10-17 01:14:05 +0400

    Initial commit

---
+++ app/src/main/java/com/example/rustem/hasher/Function.java
@@ -0,0 +1,173 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.rustem.hasher;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+/**
+ *Generationg hash values of given values with a given value
+ * @author Rustem Azimov
+ */
+public class Function {
+
+    String hashText(String text, String methodName){
+        switch(methodName){
+            case "MD5": return convertToMD5(text);
+             case "SHA1": return convertToSHA1(text);
+              case "SHA256": return convertToSHA256(text);
+               case "SHA512": return convertToSHA512(text);
+                case "BCRYPT": return convertToBCRYPT(text);
+                 case "PBKDF2": return convertToPBKDF2(text);
+                    default: return "You need support\nContact with us";
+        }
+    }
+
+    
+    /*Create a method for hasing with MD5*/
+    private String convertToMD5(String text){
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(text.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String hashedText = number.toString(16);
+            while(hashedText.length() < 32){
+                hashedText = "0" + hashedText;
+            }
+            return hashedText;
+        } catch (NoSuchAlgorithmException ex) {
+            System.out.println("No such Algorithm in MD5"); 
+        }
+        /*If te program executes the following statement
+                it means that Exception happened*/
+        return "You need support\nContact with us";
+    }
+   
+    private String convertToSHA1(String text)
+{
+    String hashedText = "";
+    try
+    {
+        MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+        crypt.reset();
+        crypt.update(text.getBytes("UTF-8"));
+  hashedText = convertByteToHex(crypt.digest());//Convert from byte to HexaDecimal then Initialize to ^hashedText^
+    }
+    catch(NoSuchAlgorithmException | UnsupportedEncodingException e)
+    {
+        //If the jvp is here  Your program is already broken :)))
+    }
+    return hashedText;
+}
+  
+  private String convertToSHA256(String base) {
+    try{
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(base.getBytes("UTF-8"));
+        StringBuilder hexString = new StringBuilder();
+
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if(hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+
+        return hexString.toString();
+    } catch(UnsupportedEncodingException | NoSuchAlgorithmException ex){
+                       //If the jvp is here  Your program is already broken :)))
+    }
+        return "You need support\nContact with us";
+}
+
+    private String convertToSHA512(String textToHash)
+    {
+        try {
+            final MessageDigest sha512 = MessageDigest.getInstance("SHA-512");
+            sha512.update(textToHash.getBytes());
+            
+            return convertByteToHex(sha512.digest());
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Function.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "You need support\nContact with us";
+    }  
+    private String convertToBCRYPT(String password_plaintext) {
+		String salt = BCrypt.gensalt(12);
+		String hashed_password = BCrypt.hashpw(password_plaintext, salt);
+
+		return(hashed_password);
+	}
+
+    private final String PBKDF2_ALGORITHM = "PBKDF2WithHmacSHA1";
+    // The following constants may be changed without breaking existing hashes.
+    private final int SALT_BYTES = 24;
+    private final int HASH_BYTES = 24;
+    private final int PBKDF2_ITERATIONS = 1000;
+
+    private final int ITERATION_INDEX = 0;
+    private final int SALT_INDEX = 1;
+    private final int PBKDF2_INDEX = 2;
+
+    /**
+     * Returns a salted PBKDF2 hash of the password.
+     *
+     * @param   password    the password to hash
+     * @return              a salted PBKDF2 hash of the password
+     */
+    public String convertToPBKDF2(String password)
+    {
+        return convertToPBKDF2(password.toCharArray());
+    }
+
+    /**
+     * Returns a salted PBKDF2 hash of the password.
+     *
+     * @param   password    the password to hash
+     * @return              a salted PBKDF2 hash of the password
+     */
+    private String convertToPBKDF2(char[] password)
+        
+    {
+        try {
+            // Generate a random salt
+            SecureRandom random = new SecureRandom();
+            byte[] salt = new byte[SALT_BYTES];
+            random.nextBytes(salt);
+            
+            // Hash the password
+            byte[] hash = pbkdf2(password, salt, PBKDF2_ITERATIONS, HASH_BYTES);
+            // format iterations:salt:hash
+            return PBKDF2_ITERATIONS + ":" + convertByteToHex(salt) + ":" +  convertByteToHex(hash);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+                           //If the jvp is here  Your program is already broken :)))
+        }
+        return "You need support\nContact with us";
+    }
+    private byte[] pbkdf2(char[] password, byte[] salt, int iterations, int bytes)
+        throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, bytes * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance(PBKDF2_ALGORITHM);
+        return skf.generateSecret(spec).getEncoded();
+    }
+    private  String convertByteToHex(byte data[])
+    {
+        StringBuilder hexData = new StringBuilder();
+        for (int byteIndex = 0; byteIndex < data.length; byteIndex++)
+            hexData.append(Integer.toString((data[byteIndex] & 0xff) + 0x100, 16).substring(1));
+
+        return hexData.toString();
+    }
+}
---
+++ app/src/main/java/com/example/rustem/hasher/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.rustem.hasher;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    /*
+     * Function class instance which contains a method for hashing with
+     * MD5, SHA-1, SHA-256, SHA-512, BCRYPT, PBKDF-2
+     */
+    private Function functions = new Function();
+
+    //Text area for giving output (hashed value of plain text
+    private EditText outputTxtView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Options for different hashing methods
+        final String[] options= new String[]
+                {
+                        "Select",
+                        "MD5",
+                        "SHA1",
+                        "SHA256",
+                        "SHA512",
+                        "BCRYPT",
+                        "PBKDF2"
+                };
+        //Specifies a method option for hashing
+        Spinner s = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, options);
+        s.setAdapter(adapter);
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String output;
+                if(position == 0)
+                {
+                    //If "select" option was selected
+                    output = "";
+                }
+                else
+                {
+
+                    output = functions.hashText(((EditText)findViewById(R.id.inputTxt)).getText().toString(), options[position]);
+                }
+                outputTxtView = (EditText)findViewById(R.id.resultView);
+                outputTxtView.setText(output);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                showMessage("Nothing was selected");
+            }
+        });
+        //Handling copy button's action
+        ((Button)findViewById(R.id.copyButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(outputTxtView != null && !outputTxtView.getText().toString().equals(""))
+                {
+                    /*
+                     * if the selected option isn't "Select" and NullPointerException won' occur
+                     * {
+                     *      copy hashed text to clipboard
+                     */
+                    copy("hashedTxt", outputTxtView.getText().toString());
+                }
+            }
+        });
+    }
+    private void showMessage(String txt){
+        Toast.makeText(MainActivity.this, txt, Toast.LENGTH_LONG).show();
+    }
+    private void copy(String label, String txt){
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText(label, txt);
+        clipboard.setPrimaryClip(clip);
+        showMessage("Copied to clipboard");
+    }
+}
